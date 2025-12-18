ff788747e2392796c093f14a51cada2b6bf5cd3d
Yeshani Hettithantri <140787733+yeshani99@users.noreply.github.com>
2025-12-18 15:26:21 +0530

    Add files via upload
    first

---
+++ BaseTest.java
@@ -0,0 +1,27 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(3000); // wait 3 seconds
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000); // wait 3 seconds before closing
+        driver.quit();
+    }
+}
+
